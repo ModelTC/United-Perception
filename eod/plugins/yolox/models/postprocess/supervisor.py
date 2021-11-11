d4dbe303a607c6fdccaf00df279606d960fcd3b1
yaoyongqiang <yaoyongqiang@sensetime.com>
2021-11-11 13:57:09 +0800

    bugfix(ota): add clip for ota

--- eod/plugins/yolox/models/postprocess/supervisor.py
+++ eod/plugins/yolox/models/postprocess/supervisor.py
@@ -176,6 +176,182 @@ class OTAMatcher(object):
         del cls_preds_
 
         cost = (
+            pair_wise_cls_loss  
+            + 3.0 * pair_wise_ious_loss
+            + 100000.0 * (~is_in_boxes_and_center)
+        )
+
+        (num_fg, gt_matched_classes, pred_ious_this_matching, matched_gt_inds
+         ) = self.dynamic_k_matching(cost, pair_wise_ious, gt_labels, G, fg_mask)
+        del pair_wise_cls_loss, cost, pair_wise_ious, pair_wise_ious_loss
+
+        if mode == "cpu":
+            gt_matched_classes = gt_matched_classes.cuda()
+            fg_mask = fg_mask.cuda()
+            pred_ious_this_matching = pred_ious_this_matching.cuda()
+            matched_gt_inds = matched_gt_inds.cuda()
+            expanded_strides = expanded_strides.cuda()
+
+        return num_fg, gt_matched_classes, pred_ious_this_matching, matched_gt_inds, fg_mask, expanded_strides
+
+    def get_sample_region(self, gt_bboxes, strides, points, num_points_per_level):
+        G = gt_bboxes.shape[0]  # num_gts
+        A = points.shape[0]     # num_anchors
+        expanded_strides = torch.zeros(1, points.shape[0]).to(dtype=points.dtype, device=points.device)
+
+        beg = 0
+        for l_ix in range(len(num_points_per_level)):
+            end = beg + num_points_per_level[l_ix]
+            expanded_strides[0][beg:end] = strides[l_ix]
+            beg += num_points_per_level[l_ix]
+
+        x_center = points[:, 0].unsqueeze(0).repeat(G, 1) + 0.5 * expanded_strides  # [G, A]
+        y_center = points[:, 1].unsqueeze(0).repeat(G, 1) + 0.5 * expanded_strides  # [G, A]
+
+        # x1, x2, y1, y2
+        gt_bboxes_l = (gt_bboxes[:, 0]).unsqueeze(1).repeat(1, A)  # [G, A]
+        gt_bboxes_r = (gt_bboxes[:, 2]).unsqueeze(1).repeat(1, A)  # [G, A]
+        gt_bboxes_t = (gt_bboxes[:, 1]).unsqueeze(1).repeat(1, A)  # [G, A]
+        gt_bboxes_b = (gt_bboxes[:, 3]).unsqueeze(1).repeat(1, A)  # [G, A]
+
+        b_l = x_center - gt_bboxes_l  # [G, A]
+        b_r = gt_bboxes_r - x_center  # [G, A]
+        b_t = y_center - gt_bboxes_t  # [G, A]
+        b_b = gt_bboxes_b - y_center  # [G, A]
+        bbox_deltas = torch.stack([b_l, b_t, b_r, b_b], 2)   # [G, A, 4]
+        is_in_boxes = bbox_deltas.min(dim=-1).values > 0.0   # [G, A]
+        is_in_boxes_all = is_in_boxes.sum(dim=0) > 0         # [A]
+
+        center_radius = self.radius
+
+        gt_bboxes_cx = (gt_bboxes[:, 0] + gt_bboxes[:, 2]) / 2
+        gt_bboxes_cy = (gt_bboxes[:, 1] + gt_bboxes[:, 3]) / 2
+
+        gt_bboxes_l = gt_bboxes_cx.unsqueeze(1).repeat(1, A) - center_radius * expanded_strides
+        gt_bboxes_r = gt_bboxes_cx.unsqueeze(1).repeat(1, A) + center_radius * expanded_strides
+        gt_bboxes_t = gt_bboxes_cy.unsqueeze(1).repeat(1, A) - center_radius * expanded_strides
+        gt_bboxes_b = gt_bboxes_cy.unsqueeze(1).repeat(1, A) + center_radius * expanded_strides
+
+        c_l = x_center - gt_bboxes_l
+        c_r = gt_bboxes_r - x_center
+        c_t = y_center - gt_bboxes_t
+        c_b = gt_bboxes_b - y_center
+        center_deltas = torch.stack([c_l, c_t, c_r, c_b], 2)
+        is_in_centers = center_deltas.min(dim=-1).values > 0.0
+        is_in_centers_all = is_in_centers.sum(dim=0) > 0
+
+        # in boxes and in centers
+        is_in_boxes_anchor = is_in_boxes_all | is_in_centers_all
+
+        is_in_boxes_and_center = (
+            is_in_boxes[:, is_in_boxes_anchor] & is_in_centers[:, is_in_boxes_anchor]
+        )
+        return is_in_boxes_anchor, is_in_boxes_and_center, expanded_strides
+
+    def dynamic_k_matching(self, cost, pair_wise_ious, gt_classes, num_gt, fg_mask):
+        matching_matrix = torch.zeros_like(cost)  # [G, A']
+
+        ious_in_boxes_matrix = pair_wise_ious    # [G, A']
+        n_candidate_k = min(self.candidate_k, ious_in_boxes_matrix.size(1))
+        topk_ious, _ = torch.topk(ious_in_boxes_matrix, n_candidate_k, dim=1)
+        dynamic_ks = torch.clamp(topk_ious.sum(1).int(), min=1)  # , max=A_-1)
+        for gt_idx in range(num_gt):
+            _, pos_idx = torch.topk(
+                cost[gt_idx], k=dynamic_ks[gt_idx].item(), largest=False
+            )
+            matching_matrix[gt_idx][pos_idx] = 1.0
+
+        del topk_ious, dynamic_ks, pos_idx
+
+        anchor_matching_gt = matching_matrix.sum(0)    # [A']  wether matched
+        if (anchor_matching_gt > 1).sum() > 0:
+            cost_min, cost_argmin = torch.min(cost[:, anchor_matching_gt > 1], dim=0)
+            matching_matrix[:, anchor_matching_gt > 1] *= 0.0
+            matching_matrix[cost_argmin, anchor_matching_gt > 1] = 1.0
+        fg_mask_inboxes = matching_matrix.sum(0) > 0.0
+        num_fg = fg_mask_inboxes.sum().item()
+
+        fg_mask[fg_mask.clone()] = fg_mask_inboxes
+
+        matched_gt_inds = matching_matrix[:, fg_mask_inboxes].argmax(0)
+        gt_matched_classes = gt_classes[matched_gt_inds]
+
+        pred_ious_this_matching = (matching_matrix * pair_wise_ious).sum(0)[fg_mask_inboxes]
+
+        return num_fg, gt_matched_classes, pred_ious_this_matching, matched_gt_inds
+
+
+@MATCHER_REGISTRY.register('ota_mot17')
+class OTAMatcherMot17(object):
+    def __init__(self, num_classes, center_sample=True, pos_radius=1, candidate_k=10, radius=2.5, img_size=[]):
+        self.pos_radius = pos_radius
+        self.center_sample = center_sample
+        self.num_classes = num_classes - 1  # 80
+        self.candidate_k = candidate_k
+        self.radius = radius
+        self.img_size = img_size
+
+    @torch.no_grad()
+    def match(self, gts, preds, points, num_points_per_level, strides, mode='cuda'):
+        ''' points: [A, 2] || gts: [G, 5] || preds: [A, 85]
+        num_points_per_level:  [15808, 3952, 988, 247, 70]
+        strides: [8, 16, 32, 64, 128]'''
+        G = len(gts)
+        if mode == 'cpu':
+            logger.info('------------CPU Mode for This Batch-------------')
+            gts = gts.cpu()
+            points = points.cpu()
+
+        gt_labels = gts[:, 4]   # [G, 1]
+        gt_bboxes = gts[:, :4]  # [G, 4]
+
+        if self.center_sample:
+            fg_mask, is_in_boxes_and_center, expanded_strides = self.get_sample_region(
+                gt_bboxes, strides, points, num_points_per_level)
+        else:
+            assert "Not implement"
+
+        if fg_mask.sum() <= 1e-6:
+            logger.info('no gt in center')
+            return 0, None, None, None, None, None
+
+        masked_preds = preds[fg_mask]
+        if mode == 'cpu':
+            masked_preds = masked_preds.cpu()
+
+        preds_cls = masked_preds[:, :self.num_classes]
+        preds_box = masked_preds[:, self.num_classes:-1]  # [A', 4]
+        preds_obj = masked_preds[:, -1:]
+
+        def decode_ota_box(preds):
+            x1 = preds[:, 0] - preds[:, 2] / 2
+            y1 = preds[:, 1] - preds[:, 3] / 2
+            x2 = preds[:, 0] + preds[:, 2] / 2
+            y2 = preds[:, 1] + preds[:, 3] / 2
+            return torch.stack([x1, y1, x2, y2], -1)
+
+        with torch.cuda.amp.autocast(enabled=False):
+            decoded_preds_box = decode_ota_box(preds_box.float())
+
+            pair_wise_ious = bbox_iou_overlaps(
+                gt_bboxes, decoded_preds_box)
+            gt_labels_ = gt_labels - 1
+            gt_cls_per_image = (
+                F.one_hot(gt_labels_.to(torch.int64), self.num_classes).float()
+                .unsqueeze(1).repeat(1, masked_preds.shape[0], 1)
+            )
+            pair_wise_ious_loss = -torch.log(pair_wise_ious + 1e-8)
+            cls_preds_ = (
+                preds_cls.float().unsqueeze(0).repeat(G, 1, 1).sigmoid_()
+                * preds_obj.float().unsqueeze(0).repeat(G, 1, 1).sigmoid_()
+            )
+            pair_wise_cls_loss = F.binary_cross_entropy(
+                cls_preds_.sqrt_(),
+                gt_cls_per_image,
+                reduction="none").sum(-1)
+        del cls_preds_
+
+        cost = (
             pair_wise_cls_loss
             + 3.0 * pair_wise_ious_loss
             + 100000.0 * (~is_in_boxes_and_center)
@@ -226,6 +402,9 @@ class OTAMatcher(object):
 
         gt_bboxes_cx = (gt_bboxes[:, 0] + gt_bboxes[:, 2]) / 2
         gt_bboxes_cy = (gt_bboxes[:, 1] + gt_bboxes[:, 3]) / 2
+
+        gt_bboxes_cx = torch.clamp(gt_bboxes_cx, min=0, max=self.img_size[1])
+        gt_bboxes_cy = torch.clamp(gt_bboxes_cy, min=0, max=self.img_size[0])
 
         gt_bboxes_l = gt_bboxes_cx.unsqueeze(1).repeat(1, A) - center_radius * expanded_strides
         gt_bboxes_r = gt_bboxes_cx.unsqueeze(1).repeat(1, A) + center_radius * expanded_strides
