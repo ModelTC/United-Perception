b6c7edf42644c0587f86f6b51c9a8d843adc6ee8
yaoyongqiang <yaoyongqiang@sensetime.com>
2021-12-12 18:28:45 +0800

    bugfix(syncbn): rm useless code

--- eod/runner/base_runner.py
+++ eod/runner/base_runner.py
@@ -532,11 +532,6 @@ class BaseRunner(object):
             model_helper_kwargs = self.config['runtime']['model_helper']['kwargs']
             model_helper_ins = MODEL_HELPER_REGISTRY[model_helper_type]
             model = model_helper_ins(net_cfg, **model_helper_kwargs)
-            if self.config['runtime']['special_bn_init']:
-                for m in model.modules():
-                    if isinstance(m, torch.nn.BatchNorm2d) or isinstance(m, torch.nn.SyncBatchNorm):
-                        m.eps = 1e-3
-                        m.momentum = 0.03
             if self.device == 'cuda':
                 model = model.cuda()
             if self.fp16 and self.backend == 'linklink':
