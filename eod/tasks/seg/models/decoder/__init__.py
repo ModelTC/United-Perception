467bf4277bff97e13921e149f2828b29126bae3f
lihongxiang <lihongxiang@sensetime.com>
2021-12-20 10:12:32 +0000

    feature(model): fix init.py

--- eod/tasks/seg/models/decoder/__init__.py
+++ eod/tasks/seg/models/decoder/__init__.py
@@ -1,2 +1,5 @@
-from .deeplab import * # noqa
-from .dfnet_decoder import * # noqa
+from .deeplab import *  # noqa
+from .dfnet_decoder import *  # noqa
+from .sfnet import *  # noqa
+from .unet_decoder import *  # noqa
+from .pspnet import *
