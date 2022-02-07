88961747832bcf5a28aa7bc82386859fc1f3fa80
baishihao <baishihao@sensetime.com>
2022-02-07 22:49:19 +0800

    feature(cls): add avg_down and droppathfor resnet

--- up/models/backbones/resnet.py
+++ up/models/backbones/resnet.py
@@ -21,6 +21,35 @@ model_urls = {
 }
 
 
+def drop_path(x, drop_prob: float = 0., training: bool = False):
+    """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
+    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
+    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
+    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
+    'survival rate' as the argument.
+    """
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+    random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+    random_tensor.floor_()  # binarize
+    output = x.div(keep_prob) * random_tensor
+    return output
+
+
+class DropPath(nn.Module):
+    """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
+    """
+    def __init__(self, drop_prob=None):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training)
+
+
 def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
     return nn.Conv2d(
@@ -42,9 +71,11 @@ class BasicBlock(nn.Module):
                  dilation=1,
                  downsample=None,
                  normalize={'type': 'solo_bn'},
-                 stride_in_1x1=False):
+                 stride_in_1x1=False,
+                 drop_path=None):
         super(BasicBlock, self).__init__()
 
+        self.drop_path = drop_path
         self.norm1_name, norm1 = build_norm_layer(planes, normalize, 1)
         self.norm2_name, norm2 = build_norm_layer(planes, normalize, 2)
 
@@ -81,6 +112,9 @@ class BasicBlock(nn.Module):
 
         out = self.conv2(out)
         out = self.norm2(out)
+        
+        if self.drop_path is not None:
+            out = self.drop_path(out)
 
         if self.downsample is not None:
             residual = self.downsample(x)
@@ -158,9 +192,11 @@ class Bottleneck(nn.Module):
                  dilation=1,
                  downsample=None,
                  normalize={'type': 'solo_bn'},
-                 stride_in_1x1=False):
+                 stride_in_1x1=False,
+                 drop_path=None):
         super(Bottleneck, self).__init__()
 
+        self.drop_path = drop_path
         self.norm1_name, norm1 = build_norm_layer(planes, normalize, 1)
         self.norm2_name, norm2 = build_norm_layer(planes, normalize, 2)
         self.norm3_name, norm3 = build_norm_layer(planes * self.expansion, normalize, 3)
@@ -219,85 +255,8 @@ class Bottleneck(nn.Module):
         out = self.conv3(out)
         out = self.norm3(out)
 
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
-class DeformBlock(nn.Module):
-    expansion = 4
-
-    def __init__(self,
-                 inplanes,
-                 planes,
-                 stride=1,
-                 dilation=2,
-                 downsample=None,
-                 normalize={'type': 'solo_bn'},
-                 stride_in_1x1=False):
-        super(DeformBlock, self).__init__()
-
-        self.norm1_name, norm1 = build_norm_layer(planes, normalize, 1)
-        self.norm2_name, norm2 = build_norm_layer(planes, normalize, 2)
-        self.norm3_name, norm3 = build_norm_layer(planes * self.expansion, normalize, 3)
-
-        stride_1x1, stride_3x3 = (stride, 1) if stride_in_1x1 else (1, stride)
-
-        self.conv1 = nn.Conv2d(
-            inplanes,
-            planes,
-            kernel_size=1,
-            stride=stride_1x1,
-            bias=False)
-        self.add_module(self.norm1_name, norm1)
-
-        kernel_size = 3
-        padding = dilation * (kernel_size - 1) // 2
-
-        self.deform_offset = nn.Conv2d(
-            planes, 18, kernel_size=3, stride=stride_3x3, padding=padding, dilation=dilation)
-        self.conv2 = DeformableConv(
-            planes, planes, kernel_size=3, stride=stride_3x3, padding=padding, dilation=dilation)
-
-        self.add_module(self.norm2_name, norm2)
-        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-        self.add_module(self.norm3_name, norm3)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    @property
-    def norm1(self):
-        return getattr(self, self.norm1_name)
-
-    @property
-    def norm2(self):
-        return getattr(self, self.norm2_name)
-
-    @property
-    def norm3(self):
-        return getattr(self, self.norm3_name)
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.norm1(out)
-        out = self.relu(out)
-
-        offset = self.deform_offset(out)
-
-        out = self.conv2(out, offset)
-
-        out = self.norm2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.norm3(out)
+        if self.drop_path is not None:
+            out = self.drop_path(out)
 
         if self.downsample is not None:
             residual = self.downsample(x)
@@ -359,7 +318,9 @@ class ResNet(nn.Module):
                  fa=None,
                  nnie=False,
                  deep_stem=False,
-                 input_channel=3):
+                 input_channel=3,
+                 drop_path_rate=0,
+                 avg_down=False):
         r"""
         Arguments:
             - frozen_layers (:obj:`list` of :obj:`int`): Index of frozen layers, [0,4]
@@ -464,13 +425,16 @@ class ResNet(nn.Module):
 
         self.inplanes = layer_out_planes[0]
         self.norm1_name, norm1 = build_norm_layer(layer_out_planes[0], normalize, 1)
+        self.block_nums = sum(layers)
+        self.drop_path_rate = drop_path_rate
+        self.avg_down = avg_down
 
         super(ResNet, self).__init__()
         if not deep_stem:
-            conv1 = nn.Conv2d(input_channel, layer_out_planes[0], kernel_size=7, stride=2, padding=3, bias=False)
+            self.conv1 = nn.Conv2d(input_channel, layer_out_planes[0], kernel_size=7, stride=2, padding=3, bias=False)
         else:
             inner_planes = layer_out_planes[0] // 2
-            conv1 = nn.Sequential(
+            self.conv1 = nn.Sequential(
                 nn.Conv2d(input_channel, inner_planes, kernel_size=3, stride=2, padding=1, bias=False),
                 build_norm_layer(inner_planes, normalize)[1],
                 nn.ReLU(inplace=True),
@@ -479,27 +443,32 @@ class ResNet(nn.Module):
                 nn.ReLU(inplace=True),
                 nn.Conv2d(inner_planes, layer_out_planes[0], kernel_size=3, stride=1, padding=1, bias=False),
             )
-        # self.add_module(self.norm1_name, norm1)
-        relu = nn.ReLU(inplace=True)
-        maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.add_module(self.norm1_name, norm1)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         if nnie:
-            maxpool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, ceil_mode=True)
-        self.layer0 = nn.Sequential(conv1, norm1, relu, maxpool)
-        self.layer1 = self._make_layer(block, layer_in_planes[1], layers[0], normalize=normalize)
+            self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0, ceil_mode=True)
+        self.layer0 = nn.Sequential(self.conv1, self.norm1, self.relu, self.maxpool)
+        self.layer1 = self._make_layer(block, layer_in_planes[1], layers[0], 
+                                       normalize=normalize, 
+                                       block_id = 0)
         self.layer2 = self._make_layer(block, layer_in_planes[2], layers[1], stride=2,
                                        normalize=normalize,
-                                       stride_in_1x1=stride_in_1x1)
+                                       stride_in_1x1=stride_in_1x1,
+                                       block_id = layers[0])
 
         self.layer3 = self._make_layer(block, layer_in_planes[3], layers[2], stride=2,
                                        normalize=normalize,
-                                       stride_in_1x1=stride_in_1x1)
+                                       stride_in_1x1=stride_in_1x1,
+                                       block_id = sum(layers[0:2]))
         if 4 in self.out_layers:
             layer4_stride = out_strides[-1] // 16
             self.layer4 = self._make_layer(block, layer_in_planes[4], layers[3],
                                            stride=layer4_stride,
                                            dilation=2 // layer4_stride,
                                            normalize=normalize,
-                                           stride_in_1x1=stride_in_1x1)
+                                           stride_in_1x1=stride_in_1x1,
+                                           block_id=sum(layers[0:3]))
         else:
             self.layer4 = None
 
@@ -534,21 +503,32 @@ class ResNet(nn.Module):
                     stride=1,
                     dilation=1,
                     normalize={'type': 'solo_bn'},
-                    stride_in_1x1=False):
+                    stride_in_1x1=False,
+                    block_id=0):
         block_types = [block] * blocks
 
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(
-                    self.inplanes,
-                    planes * block.expansion,
-                    kernel_size=1,
-                    stride=stride,
-                    bias=False),
-                build_norm_layer(planes * block.expansion, normalize)[1]
-            )
-
+            if self.avg_down:
+                downsample = nn.Sequential(
+                    nn.AvgPool2d(stride, stride=stride, ceil_mode=True, count_include_pad=False),
+                    nn.Conv2d(self.inplanes, planes * block.expansion,
+                              kernel_size=1, stride=1, bias=False),
+                    build_norm_layer(planes * block.expansion, normalize)[1],
+                )
+            else:
+                downsample = nn.Sequential(
+                    nn.Conv2d(
+                        self.inplanes,
+                        planes * block.expansion,
+                        kernel_size=1,
+                        stride=stride,
+                        bias=False),
+                    build_norm_layer(planes * block.expansion, normalize)[1]
+                )
+        block_drop_path_rate = self.drop_path_rate * block_id / (self.block_nums - 1.)
+        block_id += 1
+        block_drop_path = DropPath(block_drop_path_rate) if block_drop_path_rate > 0 else None 
         layers = []
         layers.append(block_types[0](self.inplanes,
                                      planes,
@@ -556,16 +536,22 @@ class ResNet(nn.Module):
                                      dilation,
                                      downsample,
                                      normalize,
-                                     stride_in_1x1))
+                                     stride_in_1x1,
+                                     block_drop_path))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
+            block_drop_path_rate = self.drop_path_rate * block_id / (self.block_nums - 1.)
+            block_id += 1
+            block_drop_path = DropPath(block_drop_path_rate) if block_drop_path_rate > 0 else None 
             layers.append(block_types[i](self.inplanes,
                                          planes,
                                          stride=1,
                                          dilation=dilation,
                                          downsample=None,
                                          normalize=normalize,
-                                         stride_in_1x1=stride_in_1x1))
+                                         stride_in_1x1=stride_in_1x1,
+                                         drop_path=block_drop_path
+                                         ))
         return nn.Sequential(*layers)
 
     def get_outplanes(self):
@@ -617,7 +603,7 @@ class ResNet(nn.Module):
 
     def freeze_layer(self):
         layers = [
-            self.layer0,
+            nn.Sequential(self.conv1, self.norm1, self.relu, self.maxpool),
             self.layer1, self.layer2, self.layer3, self.layer4
         ]
         for layer_idx in self.frozen_layers:
