937c0920f4ed4a0a75cd08871a734fc77b40b0bc
yaoyongqiang <yaoyongqiang@sensetime.com>
2022-02-09 13:16:13 +0800

    feature(cls_loss): add loss weight for cls loss close #4

--- up/tasks/cls/models/losses/cls_loss.py
+++ up/tasks/cls/models/losses/cls_loss.py
@@ -10,10 +10,11 @@ __all__ = ['LabelSmoothCELoss', 'BCE_LOSS']
 
 @LOSSES_REGISTRY.register('label_smooth_ce')
 class LabelSmoothCELoss(_Loss):
-    def __init__(self, smooth_ratio, num_classes):
+    def __init__(self, smooth_ratio, num_classes, loss_weight=1.0):
         super(LabelSmoothCELoss, self).__init__()
         self.smooth_ratio = smooth_ratio
         self.v = self.smooth_ratio / num_classes
+        self.loss_weight = loss_weight
 
     def forward(self, input, label):
         if label.dim() == 1:
@@ -27,14 +28,15 @@ class LabelSmoothCELoss(_Loss):
             one_hot[one_hot == 0.] += self.v
         loss = - torch.sum(F.log_softmax(input, 1) * (one_hot.detach())) / input.size(0)
 
-        return loss
+        return loss * self.loss_weight
 
 
 @LOSSES_REGISTRY.register('bce')
 class BCE_LOSS(_Loss):
-    def __init__(self):
+    def __init__(self, loss_weight=1.0):
         super().__init__()
         self.bce_loss = torch.nn.BCEWithLogitsLoss()
+        self.loss_weight = loss_weight
 
     def forward(self, input, label):
         C = input.size(1)
@@ -45,7 +47,16 @@ class BCE_LOSS(_Loss):
             loss = self.bce_loss(input, one_hot)
         elif label.dim() > 1:
             loss = self.bce_loss(input - math.log(C), label) * C
-        return loss.mean()
+        return loss.mean() * self.loss_weight
 
 
-LOSSES_REGISTRY.register('ce', torch.nn.CrossEntropyLoss)
+@LOSSES_REGISTRY.register('ce')
+class CrossEntropyLoss(_Loss):
+    def __init__(self, loss_weight=1.0):
+        super().__init__()
+        self.ce_loss = torch.nn.CrossEntropyLoss()
+        self.loss_weight = loss_weight
+
+    def forward(self, input, label):
+        loss = self.ce_loss(input, label)
+        return loss * self.loss_weight
