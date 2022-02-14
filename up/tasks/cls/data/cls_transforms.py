5c551d6f6d5a227fd7aad48f3340706ccb5426f9
baishihao <baishihao@sensetime.com>
2022-02-14 15:14:00 +0800

    feature(cls): add rand earse augmentation

--- up/tasks/cls/data/cls_transforms.py
+++ up/tasks/cls/data/cls_transforms.py
@@ -1,4 +1,6 @@
 import torch
+import math
+import random
 import numpy as np
 import torchvision.transforms as transforms
 import torchvision.transforms.functional as TF
@@ -15,7 +17,8 @@ __all__ = [
     'PILColorJitter',
     'TorchResize',
     'TorchCenterCrop',
-    'TorchMixUp']
+    'TorchMixUp',
+    'RandomErasing']
 
 
 class TorchAugmentation(Augmentation):
@@ -180,3 +183,86 @@ class TorchCutMixUp(TorchAugmentation):
         output.gt = lam * label + (1 - lam) * label_other
         output.image = data.image
         return output
+
+
+@AUGMENTATION_REGISTRY.register('torch_randerase')
+class RandomErasing(TorchAugmentation):
+    """ Randomly selects a rectangle region in an image and erases its pixels.
+        'Random Erasing Data Augmentation' by Zhong et al.
+        See https://arxiv.org/pdf/1708.04896.pdf
+        This variant of RandomErasing is intended to be applied to either a batch
+        or single image tensor after it has been normalized by dataset mean and std.
+    Args:
+         probability: Probability that the Random Erasing operation will be performed.
+         min_area: Minimum percentage of erased area wrt input image area.
+         max_area: Maximum percentage of erased area wrt input image area.
+         min_aspect: Minimum aspect ratio of erased area.
+         mode: pixel color mode, one of 'const', 'rand', or 'pixel'
+            'const' - erase block is constant color of 0 for all channels
+            'rand'  - erase block is same per-channel random (normal) color
+            'pixel' - erase block is per-pixel random (normal) color
+        max_count: maximum number of erasing blocks per image, area per box is scaled by count.
+            per-image count is randomly chosen between 1 and this value.
+    """
+
+    def __init__(
+            self,
+            probability=0.5, min_area=0.02, max_area=1 / 3, min_aspect=0.3, max_aspect=None,
+            mode='pixel', min_count=1, max_count=None, num_splits=0, device='cpu'):
+        self.probability = probability
+        self.min_area = min_area
+        self.max_area = max_area
+        max_aspect = max_aspect or 1 / min_aspect
+        self.log_aspect_ratio = (math.log(min_aspect), math.log(max_aspect))
+        self.min_count = min_count
+        self.max_count = max_count or min_count
+        self.num_splits = num_splits
+        self.mode = mode.lower()
+        self.rand_color = False
+        self.per_pixel = False
+        if self.mode == 'rand':
+            self.rand_color = True  # per block random normal
+        elif self.mode == 'pixel':
+            self.per_pixel = True  # per pixel random normal
+        else:
+            assert not self.mode or self.mode == 'const'
+        self.device = device
+
+    def _erase(self, img, chan, img_h, img_w, dtype):
+        if random.random() > self.probability:
+            return
+        area = img_h * img_w
+        count = self.min_count if self.min_count == self.max_count else \
+            random.randint(self.min_count, self.max_count)
+        for _ in range(count):
+            for attempt in range(10):
+                target_area = random.uniform(self.min_area, self.max_area) * area / count
+                aspect_ratio = math.exp(random.uniform(*self.log_aspect_ratio))
+                h = int(round(math.sqrt(target_area * aspect_ratio)))
+                w = int(round(math.sqrt(target_area / aspect_ratio)))
+                if w < img_w and h < img_h:
+                    top = random.randint(0, img_h - h)
+                    left = random.randint(0, img_w - w)
+                    img[:, top:top + h, left:left + w] = self._get_pixels(
+                        self.per_pixel, self.rand_color, (chan, h, w),
+                        dtype=dtype, device=self.device)
+                    break
+
+    def augment(self, data):
+        image = copy.copy(data).image
+        self._erase(image, *image.size(), image.dtype)
+        data.image = image
+        return data
+
+    def _get_pixels(self, per_pixel, rand_color, patch_size, dtype=torch.float32, device='cuda'):
+        if per_pixel:
+            return torch.empty(patch_size, dtype=dtype, device=device).normal_()
+        elif rand_color:
+            return torch.empty((patch_size[0], 1, 1), dtype=dtype, device=device).normal_()
+        else:
+            return torch.zeros((patch_size[0], 1, 1), dtype=dtype, device=device)
+
+    def __repr__(self):
+        fs = self.__class__.__name__ + f'(p={self.probability}, mode={self.mode}'
+        fs += f', count=({self.min_count}, {self.max_count}))'
+        return fs
