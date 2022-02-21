7dabc94eb20b3f7824ed5ca4d2ad2da2dee53c64
hujiahao1@sensetime.com <hujh960215@163.com>
2022-02-21 13:06:04 +0800

    feature(CI): update up docs to server

--- docs/update_docs.sh
+++ docs/update_docs.sh
@@ -3,7 +3,7 @@
 up=./
 export PYTHONPATH=$up:$PYTHONPATH
 
-source=doc_sph/source
-target=doc_sph/_build/html
+source=docs/source
+target=docs/_build/html
 
-sphinx-versioning build $source $target 2>&1 | tee log.doc # && sshpass -p springpypi scp -P 10000 -r $target spring@10.10.40.93:~/docs/up && echo "update up docs done"
+sphinx-versioning build $source $target 2>&1 | tee log.doc && sshpass -p springpypi scp -P 10000 -r $target spring@10.10.40.93:~/docs/up && echo "update up docs done"
