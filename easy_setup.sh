8dbeb1cf2c8e4405cc95ef5b65703a72c63561a5
yaoyongqiang <yaoyongqiang@sensetime.com>
2022-02-25 20:52:55 +0800

    doc(setup): update for 1424 setup.sh

--- easy_setup.sh
+++ easy_setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 export PATH=/mnt/lustre/share/gcc/gcc-5.3.0/bin/:$PATH
+# for 1424 export PATH=/mnt/lustre/share/cuda-11.2/bin:/mnt/lustre/share/gcc/gcc-5.3.0/bin:$PATH
 export LD_LIBRARY_PATH=/mnt/lustre/share/gcc/gmp-4.3.2/lib:/mnt/lustre/share/gcc/mpfr-2.4.2/lib:/mnt/lustre/share/gcc/mpc-0.8.1/lib:$LD_LIBRARY_PATH
 export TORCH_CUDA_ARCH_LIST='3.5;5.0+PTX;6.0;7.0'
+# export TORCH_CUDA_ARCH_LIST='3.5;5.0+PTX;6.0;7.0;8.0;8.6'
+# export MAX_JOBS=8
 
 pip uninstall -y nart_tools
 pip uninstall -y nart==0.2.4
