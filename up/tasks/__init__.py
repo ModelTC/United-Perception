535eee65222d2b8f4bea1cd5555c9496f89d8d3a
yaoyongqiang <yaoyongqiang@sensetime.com>
2022-03-09 21:52:45 +0800

    feature(tasks): support dynamic import

--- up/tasks/__init__.py
+++ up/tasks/__init__.py
@@ -1,6 +1,16 @@
-from .det import * # noqa
-from .cls import * # noqa
-from .seg import * # noqa
-from .kp import * # noqa
-from .multitask import *  # noqa
-from .det_3d import *  # noqa
+import importlib
+import os
+
+
+def is_package(dirname):
+    return os.path.exists(os.path.join(dirname, '__init__.py'))
+
+
+pwd = os.path.dirname(os.path.realpath(__file__))
+tasks_names = os.environ.get("DEFAULT_TASKS", os.listdir(pwd))
+if ':' in tasks_names:
+    tasks_names = tasks_names.split(':')
+for fp in tasks_names:
+    realpath = os.path.join(pwd, fp)
+    if os.path.isdir(realpath) and is_package(realpath):
+        globals()[fp] = importlib.import_module('.' + fp, __package__)
