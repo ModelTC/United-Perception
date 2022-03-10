b9daebd27c3f8f32388013ecb48386b35a601de2
yaoyongqiang <yaoyongqiang@sensetime.com>
2022-03-10 09:58:32 +0800

    bugfix(tasks): fix type error

--- up/tasks/__init__.py
+++ up/tasks/__init__.py
@@ -9,10 +9,13 @@ def is_package(dirname):
 pwd = os.path.dirname(os.path.realpath(__file__))
 tasks_names = os.environ.get("DEFAULT_TASKS", os.listdir(pwd))
 exclude_tasks = os.environ.get("EXCLUDE_TASKS", '').split(":")
+if not isinstance(exclude_tasks, list):
+    exclude_tasks = [exclude_tasks]
 exclude_tasks = set(exclude_tasks)
-
 if ':' in tasks_names:
     tasks_names = tasks_names.split(':')
+if not isinstance(tasks_names, list):
+    tasks_names = [tasks_names]
 for fp in tasks_names:
     if fp in exclude_tasks:
         continue
