cb0813763cdcf737a1719097c5d427b8b319a909
yaoyongqiang <yaoyongqiang@sensetime.com>
2022-03-23 15:10:17 +0800

    refactor(cls): refactor multi cls models

--- up/utils/model/normalize.py
+++ up/utils/model/normalize.py
@@ -1,6 +1,7 @@
 # Import from third library
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from up.utils.general.global_flag import DIST_BACKEND
 
 from up.utils.model.bn_helper import (
@@ -111,3 +112,31 @@ def build_conv_norm(in_channels,
         if not relu_first:
             seq.add_module('relu', nn.ReLU(inplace=True))
     return seq
+
+
+class LayerNorm(nn.Module):
+    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first.
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs
+    with shape (batch_size, channels, height, width).
+    """
+
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError
+        self.normalized_shape = (normalized_shape,)
+
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
