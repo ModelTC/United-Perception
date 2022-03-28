6a7c3a31e7c5c602230618d55aaefa5217686e10
wushuo <wushuo@sensetime.com>
2022-03-28 11:51:42 +0800

    bugfix(inference): fix bug for sync_bn inference close #33

--- up/commands/inference.py
+++ up/commands/inference.py
@@ -3,13 +3,18 @@ from __future__ import division
 # Standard Library
 import os
 import argparse
+import torch.multiprocessing as mp
+import sys
 
 # Import from local
 from .subcommand import Subcommand
 from up.utils.general.yaml_loader import load_yaml  # IncludeLoader
 from up.utils.general.registry_factory import SUBCOMMAND_REGISTRY, INFERENCER_REGISTRY
 from up.utils.general.user_analysis_helper import send_info
-
+from up.utils.general.global_flag import DIST_BACKEND
+from up.utils.env.launch import launch
+from up.utils.env.dist_helper import setup_distributed
+from up.utils.general.log_helper import default_logger as logger
 
 __all__ = ['Inference']
 
@@ -20,6 +25,38 @@ class Inference(Subcommand):
         sub_parser = parser.add_parser(name,
                                        description='subcommand for inferencing',
                                        help='inference an image')
+        sub_parser.add_argument(
+            '--fork-method',
+            dest='fork_method',
+            type=str,
+            default='fork',
+            choices=['spawn', 'fork'],
+            help='method to fork subprocess, especially for dataloader')
+        sub_parser.add_argument('--backend',
+                                dest='backend',
+                                type=str,
+                                default='linklink',
+                                help='model backend')
+        sub_parser.add_argument('--ng', '--num_gpus_per_machine',
+                                dest='num_gpus_per_machine',
+                                type=int,
+                                default=8,
+                                help='num_gpus_per_machine')
+        sub_parser.add_argument('--nm', '--num_machines',
+                                dest='num_machines',
+                                type=int,
+                                default=1,
+                                help='num_machines')
+        sub_parser.add_argument('--launch',
+                                dest='launch',
+                                type=str,
+                                default='slurm',
+                                help='launch backend')
+        sub_parser.add_argument('--port',
+                                dest='port',
+                                type=int,
+                                default=13333,
+                                help='dist port')
         sub_parser.add_argument('--config',
                                 dest='config',
                                 required=True,
@@ -77,4 +114,16 @@ def main(args):
 
 
 def _main(args):
-    main(args)
+    DIST_BACKEND.backend = args.backend
+    if args.launch == 'pytorch':
+        launch(main, args.num_gpus_per_machine, args.num_machines, args=args, start_method=args.fork_method)
+    else:
+        mp.set_start_method(args.fork_method, force=True)
+        fork_method = mp.get_start_method(allow_none=True)
+        assert fork_method == args.fork_method
+        sys.stdout.flush()
+        try:
+            setup_distributed(args.port, args.launch, args.backend)
+        except KeyError:
+            logger.info("Setup distributed env failed.")
+        main(args)
