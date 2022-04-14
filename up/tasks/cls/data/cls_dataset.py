d10a6c622eff8d4103d13997f01de1c2b09be4e9
yaoyongqiang <yaoyongqiang@sensetime.com>
2022-04-14 16:15:59 +0800

    feature(cls_data): add rank_cls dataset

--- up/tasks/cls/data/cls_dataset.py
+++ up/tasks/cls/data/cls_dataset.py
@@ -1,11 +1,13 @@
 import json
 import numpy as np
+import math
 from up.data.datasets.base_dataset import BaseDataset
 from up.utils.general.registry_factory import DATASET_REGISTRY
 from easydict import EasyDict
 from PIL import Image
 from up.utils.general.petrel_helper import PetrelHelper
 from up.data.data_utils import is_numpy_image
+from up.utils.env.dist_helper import env
 from up.utils.general.registry import Registry
 
 __all__ = ['ClsDataset']
@@ -23,9 +25,12 @@ class BaseParser(object):
 
 @CLS_PARSER_REGISTRY.register('imagenet')
 class ImageNetParser(BaseParser):
-    def parse(self, meta_file, idx, metas):
+    def parse(self, meta_file, idx, metas, start_index=0, rank_indices=None):
         with PetrelHelper.open(meta_file) as f:
-            for line in f:
+            for index, line in enumerate(f):
+                if rank_indices is not None:
+                    if (start_index + index) not in rank_indices:
+                        continue
                 cls_res = {}
                 filename, label = line.strip().split()
                 cls_res['filename'] = filename
@@ -37,9 +42,12 @@ class ImageNetParser(BaseParser):
 
 @CLS_PARSER_REGISTRY.register('custom_cls')
 class CustomClsParser(BaseParser):
-    def parse(self, meta_file, idx, metas):
+    def parse(self, meta_file, idx, metas, start_index=0, rank_indices=None):
         with PetrelHelper.open(meta_file) as f:
-            for line in f:
+            for index, line in enumerate(f):
+                if rank_indices is not None:
+                    if (start_index + index) not in rank_indices:
+                        continue
                 cls_res = {}
                 res = json.loads(line.strip())
                 filename = res['filename']
@@ -55,10 +63,13 @@ class CustomClsParser(BaseParser):
 
 @CLS_PARSER_REGISTRY.register("custom_det")
 class CustomDetParser(BaseParser):
-    def parse(self, meta_file, idx, metas):
+    def parse(self, meta_file, idx, metas, start_index=0, rank_indices=None):
         min_size = self.extra_info.get("min_size", 40)
         with PetrelHelper.open(meta_file) as f:
-            for line in f:
+            for index, line in enumerate(f):
+                if rank_indices is not None:
+                    if (start_index + index) not in rank_indices:
+                        continue
                 data = json.loads(line)
                 for instance in data.get("instances", []):
                     if instance.get('is_ignored', False):
@@ -192,3 +203,94 @@ class ClsDataset(BaseDataset):
             }
             out_res.append(res)
         return out_res
+
+
+@DATASET_REGISTRY.register('rank_cls')
+class RankClsDataset(ClsDataset):
+    def __init__(self,
+                 meta_file,
+                 image_reader,
+                 transformer=None,
+                 evaluator=None,
+                 meta_type='imagenet',
+                 parser_info={},
+                 image_type='pil',
+                 reload_cfg={}):
+        self.mini_epoch = reload_cfg.get('mini_epoch', 1)
+        self.seed = reload_cfg.get('seed', 0)
+        self.mini_epoch_idx = reload_cfg.get('mini_epoch_idx', 0)
+        self.group = reload_cfg.get('group', 1)
+        self.world_size = env.world_size
+        self.rank = env.rank
+        super(ClsDataset, self).__init__(meta_file,
+                                         image_reader,
+                                         transformer,
+                                         evaluator,
+                                         meta_type,
+                                         parser_info,
+                                         image_type)
+
+    def count_dataset_size(self, meta_files):
+        from itertools import (takewhile, repeat)
+        buffer = 1024 * 1024 * 8
+        self.dataset_sizes = []
+        for meta_file in meta_files:
+            meta_size = 0
+            with PetrelHelper.open(meta_file) as f:
+                try:
+                    buf_gen = takewhile(lambda x: x, (f.read(buffer) for _ in repeat(None)))
+                    size = sum(buf.count('\n') for buf in buf_gen)
+                except Exception as e:  # noqa
+                    size = 0
+                    for _ in f:
+                        size += 1
+                meta_size += size
+            self.dataset_sizes.append(meta_size)
+        return np.array(self.dataset_sizes).sum()
+
+    def get_rank_indices(self, meta_files):
+        dataset_size = self.count_dataset_size(meta_files)
+        indices = self.get_group_random_indices(dataset_size, group=self.group)
+        rank_num_samples = int(math.ceil(dataset_size * 1.0 / (self.world_size * self.mini_epoch)))
+        total_size = rank_num_samples * self.world_size * self.mini_epoch
+        indices += indices[:(total_size - len(indices))]
+        offset = rank_num_samples * self.rank
+        mini_epoch_offset_begin = self.mini_epoch_idx * rank_num_samples * self.world_size
+        mini_epoch_offset_end = (self.mini_epoch_idx + 1) * rank_num_samples * self.world_size
+        rank_indices = indices[mini_epoch_offset_begin:mini_epoch_offset_end][offset:offset + rank_num_samples]
+        assert len(rank_indices) == rank_num_samples
+        return rank_indices, rank_num_samples
+
+    def get_group_random_indices(self, dataset_size, group=1):
+        magic_num = 10000
+        indices = []
+        temp_indices = []
+        mini_dataset_size = int(math.ceil(dataset_size * 1.0 / group))
+        group = math.ceil(dataset_size * 1.0 / mini_dataset_size)
+        last_size = dataset_size - mini_dataset_size * (group - 1)
+        for i in range(group):
+            if i <= group - 2:
+                cur_size = mini_dataset_size
+            else:
+                cur_size = last_size
+            np.random.seed(self.seed + i + magic_num)
+            _indices = np.random.permutation(cur_size).astype(np.int32)
+            _indices += i * mini_dataset_size
+            temp_indices.append(_indices.tolist())
+        np.random.seed(self.seed + magic_num)
+        for i in np.random.permutation(group):
+            indices.extend(temp_indices[i])
+        return indices
+
+    def parse_metas(self):
+        rank_indices, rank_num_samples = self.get_rank_indices(self.meta_file)
+        rank_indices = set(rank_indices)
+        self.metas = []
+        start_indexs = [0]
+        # acc sum
+        for idx in range(1, len(self.dataset_sizes)):
+            start_indexs.append(start_indexs[idx - 1] + self.dataset_sizes[idx - 1])
+        for idx, meta_file in enumerate(self.meta_file):
+            self.meta_parser[idx].parse(meta_file, idx, self.metas, start_indexs[idx], rank_indices)
+        if len(rank_indices) != rank_num_samples:
+            self.metas += self.metas[:(rank_num_samples - len(rank_indices))]
