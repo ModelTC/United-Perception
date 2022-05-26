edd0d1e70817d2166cb865ca8630b02edc2d2a37
zhuchengkai <zhuchengkai@sensetime.com>
2022-05-26 15:17:38 +0800

    Fix key replace output

--- up/tasks/multitask/models/wrappers/multitask_wrapper.py
+++ up/tasks/multitask/models/wrappers/multitask_wrapper.py
@@ -58,7 +58,7 @@ def key_replace_wrapper(module, cfg={}):
             inputs[v] = inputs.pop(k)
         outputs = self._forward(inputs)
         for k, v in self.outpu_replace_dict.items():
-            outputs[v] = outputs[k]
+            outputs[v] = outputs.pop(k)
         return outputs
 
     if isinstance(module, dict):
