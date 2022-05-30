12139a9933032a6a01a74e0da1ff5d8542abecce
hujiahao1 <hujiahao1@sensetime.com>
2022-05-30 20:26:43 +0800

    style(docs): mount readthe.docs

--- docs/update_docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-up=./
-export PYTHONPATH=$up:$PYTHONPATH
-
-source=docs/source
-target=docs/_build/html
-
-sphinx-versioning build $source $target 2>&1 | tee log.doc && sshpass -p springpypi scp -P 10000 -r $target spring@10.10.40.93:~/docs/up && echo "update up docs done"
