89da4c7d109523d93e094084723ac84091f0da4f
yongyang <yongyang@sensetime.com>
2022-06-28 14:21:08 +0800

    [Feature] quant deploy
    [Fix] quant deploy bug

--- up/commands/__init__.py
+++ up/commands/__init__.py
@@ -1,4 +1,5 @@
 from .train import Train  # noqa
 from .inference import Inference # noqa
 from .eval import Eval # noqa
+from .quant_deploy import QuantDeploy # noqa
 from .flops import Flops # noqa
