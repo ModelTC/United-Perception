2f5c8bc8549cc16810d093050fe6d0aa5b902629
zhangfeizhao <zhangfeizhao@sensetime.com>
2022-08-25 09:39:33 +0800

    fixbug(giou): generalized_box_iou

--- up/tasks/det/models/utils/bbox_helper.py
+++ up/tasks/det/models/utils/bbox_helper.py
@@ -99,28 +99,31 @@ def vanilla_bbox_iou_overlaps(b1, b2, aligned=False, return_union=False):
 
 
 @allow_empty_tensor(2)
-def generalized_box_iou(boxes1, boxes2, return_iou=False):
-    """
-    Generalized IoU from https://giou.stanford.edu/
-
-    The boxes should be in [x0, y0, x1, y1] format
-
-    Returns a [N, M] pairwise matrix, where N = len(boxes1)
-    and M = len(boxes2)
-    """
-    # degenerate boxes gives inf / nan results
-    # so do an early check
+def generalized_box_iou(boxes1, boxes2):
     assert (boxes1[:, 2:] >= boxes1[:, :2]).all()
     assert (boxes2[:, 2:] >= boxes2[:, :2]).all()
-    iou, union = bbox_iou_overlaps(boxes1, boxes2, return_union=True)  # box_iou(boxes1, boxes2)
-    lt = torch.min(boxes1[:, None, :2], boxes2[:, :2])
-    rb = torch.max(boxes1[:, None, 2:], boxes2[:, 2:])
-    wh = (rb - lt).clamp(min=0)  # [N,M,2]
-    area = wh[:, :, 0] * wh[:, :, 1]
-    if return_iou:
-        return iou - (area - union) / area, iou
-    else:
-        return iou - (area - union) / area
+
+    from torchvision.ops.boxes import box_area
+    area1 = box_area(boxes1)
+    area2 = box_area(boxes2)
+
+    lt_max = torch.max(boxes1[:, None, :2], boxes2[:, :2])  # [N,M,2]
+    rb_min = torch.min(boxes1[:, None, 2:], boxes2[:, 2:])  # [N,M,2]
+
+    wh_in = (rb_min - lt_max).clamp(min=0)  # [N,M,2]
+    inter = wh_in[:, :, 0] * wh_in[:, :, 1]  # [N,M]
+
+    union = area1[:, None] + area2 - inter
+
+    iou = inter / union
+
+    lt_min = torch.min(boxes1[:, None, :2], boxes2[:, :2])
+    rb_max = torch.max(boxes1[:, None, 2:], boxes2[:, 2:])
+
+    wh_ex = (rb_max - lt_min).clamp(min=0)  # [N,M,2]
+    area = wh_ex[:, :, 0] * wh_ex[:, :, 1]
+
+    return iou - (area - union) / area
 
 
 @allow_empty_tensor(2)
