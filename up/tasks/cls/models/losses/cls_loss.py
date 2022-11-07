8148266ee4f1ba1706f329672d424c61db3e3105
yaoyongqiang <yaoyongqiang@sensetime.com>
2022-11-07 13:31:28 +0800

    bugfix(loss): fix eql loss

--- up/tasks/cls/models/losses/cls_loss.py
+++ up/tasks/cls/models/losses/cls_loss.py
@@ -115,6 +115,58 @@ class SoftmaxEQLLoss(_Loss):
         self.pos_neg = self.pos_grad / (self.neg_grad + 1e-10)
 
 
+@LOSSES_REGISTRY.register('softmax_eqlv2')
+class SoftmaxEQLV2Loss(_Loss):
+    def __init__(self, num_classes, indicator='pos', loss_weight=1.0, tau=1.0, eps=1e-4):
+        super(SoftmaxEQLV2Loss, self).__init__()
+        self.loss_weight = loss_weight
+        self.num_classes = num_classes
+        self.tau = tau
+        self.eps = eps
+
+        assert indicator in ['pos', 'neg', 'pos_and_neg'], 'Wrong indicator type!'
+        self.indicator = indicator
+
+        # initial variables
+        self.register_buffer('pos_grad', torch.zeros(num_classes))
+        self.register_buffer('neg_grad', torch.zeros(num_classes))
+        self.register_buffer('pos_neg', torch.ones(num_classes))
+
+    def forward(self, input, label):
+        if self.indicator == 'pos':
+            indicator = self.pos_grad.detach()
+        elif self.indicator == 'neg':
+            indicator = self.neg_grad.detach()
+        elif self.indicator == 'pos_and_neg':
+            indicator = self.pos_neg.detach() + self.neg_grad.detach()
+        else:
+            raise NotImplementedError
+
+        if label.dim() == 1:
+            one_hot = F.one_hot(label, self.num_classes)
+        else:
+            one_hot = label.clone()
+        self.targets = one_hot.detach()
+
+        indicator = indicator / (indicator.sum() + self.eps)
+        indicator = (indicator ** self.tau + 1e-9).log()
+        cls_score = input + indicator[None, :]
+        loss = F.cross_entropy(cls_score, label)
+        return loss * self.loss_weight
+
+    def collect_grad(self, grad):
+        grad = torch.abs(grad)
+        pos_grad = torch.sum(grad * self.targets, dim=0)
+        neg_grad = torch.sum(grad * (1 - self.targets), dim=0)
+
+        allreduce(pos_grad)
+        allreduce(neg_grad)
+
+        self.pos_grad += pos_grad
+        self.neg_grad += neg_grad
+        self.pos_neg = self.pos_grad / (self.neg_grad + 1e-10)
+
+
 @LOSSES_REGISTRY.register('stce')
 class SoftTargetCrossEntropy(_Loss):
 
